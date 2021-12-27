47cd983642d9a95c909be7c0035fdce1e2207c70
Adam Brown <adamarthurryan@gmail.com>
2021-12-27 00:18:07 -0400

    solve p72

---
+++ p72/src/main.rs
@@ -0,0 +1,17 @@
+use primes::TotientSieve;
+
+#[test]
+fn solves() {
+    assert_eq!(solve(8),21);
+    assert_eq!(solve(1_000_000),303963552391);
+}
+
+fn main() {
+    println!("solve(8): {}", solve(8));
+    println!("Solution: {}", solve(1_000_000));
+}
+
+fn solve(max_d: usize) -> usize {
+    let sieve = TotientSieve::new(max_d);
+    return (2..=max_d).map(|d| sieve.totient(d)).sum();
+}
