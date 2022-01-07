e7fb2b49c6f0dcde5f82edc23ab2bda59dd62d3a
Adam Brown <adamarthurryan@gmail.com>
2022-01-07 18:57:15 -0400

    start continued fractions lib

--- p78/src/main.rs
+++ p78/src/main.rs
@@ -2,12 +2,28 @@
 Find the least value of n for which p(n) is divisible by one million.
 */
 
-//maybe this would be faster with the Euler transform
+//use the Euler tranform, similar to p76, but with streamlined memo
+//this should use the pentagonal number expansion
 
 use memoize::memoize;
 
 #[test]
-fn solves() {
+fn p_mod_million_works() {
+    //the correct digits of the sequence
+    let correct = [1,1,2,3,5,7,11,15,22,30,42,56];
+    let mut memo = vec![vec![1;1];1];
+
+    for n in 2..10 {
+        memo.push(Vec::with_capacity(n));
+
+        let mut sum = 0;
+        for k in 1..=n {
+            let p = p_mod_million(k, n, &memo);
+            sum += p;
+            memo[n-1].push(p);
+        }
+        assert_eq!(sum, correct[n]);
+    }
 }
 
 fn main() {
@@ -15,13 +31,23 @@ fn main() {
 }
 
 fn solve() -> usize {
+    let mut memo = vec![vec![1;1];1];
+
     for n in 2.. {
+        memo.push(Vec::with_capacity(n));
+
         let mut sum = 0;
-        for k in 0..n {
-            sum = (sum + p_mod_million(k as isize, n as isize))% 1_000_000;
+        for k in 1..=n {
+            let p = p_mod_million(k, n, &memo);
+            memo[n-1].push(p);
+            sum += p;
+            if sum>1_000_000 {sum -= 1_000_000; }
         }
-        println!("n:{:>10}, sum:{:>10}", n, sum);
-        if (sum+1)%1_000_000 == 0 {
+
+        if n%100 == 0 {
+            println!("n:{:>10}, sum:{:>10}", n, sum);            
+        }
+        if sum%1_000_000 == 0 {
             return n;
         }    
     }
@@ -29,10 +55,13 @@ fn solve() -> usize {
     unreachable!();
 }
 
-#[memoize]
-fn p_mod_million(k:isize, n: isize) ->  usize {
-    if k==0 && n==0 { return 1; }
+fn p_mod_million(k:usize, n: usize, memo: &Vec<Vec<usize>>) ->  usize {
+    //the result is correct for n+1... not for n...
+    if k==1 { return 1; }
     else if k<=0 || n<=0 { return 0; }
 
-    return (p_mod_million(k, n-k) + p_mod_million(k-1, n-1)) % 1_000_000;
+    let a = if n-k>=k {memo[n-k-1][k-1]} else {0};
+    let b = if n>1 && k>1 {memo[n-1-1][k-1-1]} else {1};
+
+    return if a+b>1_000_000 {a+b-1_000_000} else {a+b};
 }
